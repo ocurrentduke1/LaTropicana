e25c086a42123473315a3d26ec73b0b77d24341e
ocurrentduke1 <a21110128@ceti.mx>
2023-11-08 15:20:12 -0600

    update Login

--- app/src/main/java/com/example/prueba2/LoginActivity.java
+++ app/src/main/java/com/example/prueba2/LoginActivity.java
@@ -44,7 +44,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
-        //herramientas Google
+        //herramientas Google.
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
